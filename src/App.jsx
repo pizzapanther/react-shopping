dddf767c1878a465e8ddd5eaf57f3573608ad9a2
Paul Bailey <paul.m.bailey@gmail.com>
2021-08-07 11:15:15 -0500

    material ui setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+import { createTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
 import ShoppingList from './shopping-list.jsx';
 import AddItem from './add-item.jsx';
 import store from './store.js';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
 
 const NoMatch = ({ location }) => (
   <div>
@@ -15,18 +38,23 @@ const NoMatch = ({ location }) => (
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <div className="App">
-          <header>
-            <h1><Link to="/">Shopping List</Link></h1>
-          </header>
-          <Switch>
-            <Route exact path="/" component={ShoppingList}/>
-            <Route path="/add" component={AddItem}/>
-            <Route component={NoMatch}/>
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <div className="App">
+            <AppBar position="relative">
+              <Toolbar>
+                <h1><Link to="/" style={{textDecoration: 'none', color: 'white'}}>Shopping List</Link></h1>
+              </Toolbar>
+            </AppBar>
+            <Switch>
+              <Route exact path="/" component={ShoppingList}/>
+              <Route path="/add" component={AddItem}/>
+              <Route component={NoMatch}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   );
 }
--- src/shopping-list.jsx
+++ src/shopping-list.jsx
@@ -1,19 +1,41 @@
 import react from 'react';
 import { connect } from 'react-redux';
 
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
 class ShoppingList extends (react.Component) {
 	render() {
 		return (
-			<div>
-				<ul>
-					{this.props.shopping_list.map((item, index) => {
-					  return <li key={index}>
-					    <input type="checkbox"/>
-					    {item.name}
-					  </li>;
-					})}
-				</ul>
-			</div>
+			<Card style={{maxWidth: '500px', margin: '30px auto'}}>
+				<CardContent>
+					<List>
+						{this.props.shopping_list.map((item, index) => {
+						  return <ListItem key={index} dense divider>
+						    <FormControlLabel
+					        control={
+					          <Checkbox
+					            checked={item.in_cart}
+					            // onChange={handleChange}
+					            color="primary"
+					          />
+					        }
+					        label={item.name}
+					      />
+						  </ListItem>;
+						})}
+					</List>
+				</CardContent>
+				<CardActions>
+					<Button>Add Item</Button>
+				</CardActions>
+			</Card>
 		);
 	}
 }
